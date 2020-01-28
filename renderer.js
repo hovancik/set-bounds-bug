b00d78c1d9f0d30f661f7dd0ac4ffe16b11c4bb2
Jan Hovancik <conta.srdr@gmail.com>
2020-01-28 09:10:47 +0100

    Init bug

--- renderer.js
+++ renderer.js
@@ -4,3 +4,30 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const { remote } = require('electron')
+
+const bounds = remote.getCurrentWindow().getBounds()
+console.log(bounds)
+
+
+document.querySelector('#one').onclick = event => {
+  console.log(bounds.y)
+  remote.getCurrentWindow().setBounds({ height: 500 })
+  // Workaround-ish remote.getCurrentWindow().setBounds({ x: bounds.x, y: bounds.y, width: bounds.width, height: 500 })
+  console.log(remote.getCurrentWindow().getBounds())
+}
+
+document.querySelector('#two').onclick = event => {
+  console.log(bounds.y)
+  remote.getCurrentWindow().setBounds({ height: 400 })
+  // Workaround-ish remote.getCurrentWindow().setBounds({ x: bounds.x, y: bounds.y, width: bounds.width, height: 400 })
+  console.log(remote.getCurrentWindow().getBounds())
+}
+
+document.querySelector('#three').onclick = event => {
+  console.log(bounds.y)
+  remote.getCurrentWindow().setBounds({ height: 300 })
+  // Workaround-ish remote.getCurrentWindow().setBounds({ x: bounds.x, y: bounds.y, width: bounds.width, height: 300 })
+  console.log(remote.getCurrentWindow().getBounds())
+}
