4f40a4286bdde589b0b1c7d4213e26cb2381b203
Jan Hovancik <conta.srdr@gmail.com>
2021-03-09 12:36:36 +0100

    Up to Electron 12

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+require('@electron/remote/main').initialize()
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -12,7 +14,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      preload: path.join(__dirname, 'preload.js'),
+      enableRemoteModule: true
     }
   })
 
--- preload.js
+++ preload.js
@@ -9,4 +9,28 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  const bounds = require('@electron/remote').getCurrentWindow().getBounds()
+  console.log(bounds)
+
+  document.querySelector('#one').onclick = event => {
+    console.log(bounds.y)
+    require('@electron/remote').getCurrentWindow().setBounds({ height: 500 })
+    // Workaround-ish require('@electron/remote').getCurrentWindow().setBounds({ x: bounds.x, y: bounds.y, width: bounds.width, height: 500 })
+    console.log(require('@electron/remote').getCurrentWindow().getBounds())
+  }
+
+  document.querySelector('#two').onclick = event => {
+    console.log(bounds.y)
+    require('@electron/remote').getCurrentWindow().setBounds({ height: 400 })
+    // Workaround-ish require('@electron/remote').getCurrentWindow().setBounds({ x: bounds.x, y: bounds.y, width: bounds.width, height: 400 })
+    console.log(require('@electron/remote').getCurrentWindow().getBounds())
+  }
+
+  document.querySelector('#three').onclick = event => {
+    console.log(bounds.y)
+    require('@electron/remote').getCurrentWindow().setBounds({ height: 300 })
+    // Workaround-ish require('@electron/remote').getCurrentWindow().setBounds({ x: bounds.x, y: bounds.y, width: bounds.width, height: 300 })
+    console.log(require('@electron/remote').getCurrentWindow().getBounds())
+  }
 })
